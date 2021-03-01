4cd03d07b20a85890d6e2f170e8373c8fe46057a
Sora <reversionmeteor@gmail.com>
2021-03-01 16:26:51 +0800

    chapter5 exercise

--- chapter5/exercise/11.c
+++ chapter5/exercise/11.c
@@ -6,15 +6,21 @@ int main(void)
     printf("Enter the area code: ");
     scanf("%d", &area_code);
     switch(area_code) {
-        case 229: { printf("Albany\n"); break; }
-        case 404: printf("Atlanta\n"); break;
-        case 470: printf("Atlanta\n"); break;
-        case 478: printf("Macon\n"); break;
-        case 678: printf("Atlanta\n"); break;
-        case 706: printf("Columbus\n"); break;
-        case 762: printf("Columbus\n"); break;
-        case 770: printf("Atlanta\n"); break;
-        case 912: printf("Savannah\n"); break;
+        case 229: 
+            printf("Albany\n"); 
+            break;
+        case 404: case 470:  case 678: case 770: 
+            printf("Atlanta\n"); 
+            break;
+        case 478: 
+            printf("Macon\n"); 
+            break;
+        case 706: case 762: 
+            printf("Columbus\n"); 
+            break;
+        case 912: 
+            printf("Savannah\n"); 
+            break;
         default: printf("Area code not recognized\n");
     }
 
---
+++ chapter5/exercise/exerciese.md
@@ -0,0 +1,22 @@
+1.(a) 1 (b) 1 (c) 1 (d) 0
+
+2.(a) 1 (b) 1 (c) 1 (d) 1
+
+3.(a) 1; 3, 4, 5
+  (b) 0; 7, 8, 9
+  (c) 1; 8, 8, 9
+  (d) 1; 2, 1, 1
+
+4.(i > j) - (i < j)
+
+5.always run
+
+6.legal but confusing
+
+7.print 17 in both case
+
+8.teenager = (age >= 13) && (teenager <= 19)
+
+9.equivalent
+
+10.onetwo
